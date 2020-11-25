933f9fda26a7291b3e81bbe2c3fb3eb1eb40d8e3
victorpyatakov <victor.pyatakov95@yandex.ru>
2020-11-25 21:52:33 +0300

    first commit

---
+++ firstapp/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+
+
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
+
+
+def products(request, product_id=1):
+    output = "<h2>Product # {0}</h2>".format(product_id)
+    return HttpResponse(output)
+
+
+def users(request, id=1, name='Victor'):
+    output = "<h2>User</h2><h3>id: {0} name {1}</h3>".format(id, name)
+    return HttpResponse(output)
