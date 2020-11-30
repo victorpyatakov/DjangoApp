0b1e58934eb5a6adfe7b0b6bf6dc4fa0f05d4775
victorpyatakov <victor.pyatakov95@yandex.ru>
2020-11-30 18:43:11 +0300

    Создание и использование шаблонов

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,19 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
-
+from django.shortcuts import render
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Index")
-
-
-def about(request):
-    return HttpResponse("About")
-
-
-def contact(request):
-    return HttpResponseRedirect("/about")
-
-
-def details(request):
-    return HttpResponsePermanentRedirect("/")
+    return render(request, "firstapp/home.html")
--- hello/urls.py
+++ hello/urls.py
@@ -21,7 +21,4 @@ from firstapp import views
 
 urlpatterns = [
     path("", views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
-    path('details/', views.details)
 ]
