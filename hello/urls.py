5dea705c672c46e73b10358e57c28ee54f83d7b7
victorpyatakov <victor.pyatakov95@yandex.ru>
2020-12-02 21:31:17 +0300

    TemplateView

--- hello/urls.py
+++ hello/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 from firstapp import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", views.index),
+    path('about/', TemplateView.as_view(template_name='about.html', extra_context={'header': "О сайте"})),
+    path('contact/', TemplateView.as_view(template_name='concact.html')),
 ]
